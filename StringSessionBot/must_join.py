265379958e2461c2970d23f344b0d3f53c76318e
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-02 22:59:21 +0300

    Update must_join.py

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -13,13 +13,13 @@ async def must_join_channel(bot: Client, msg: Message):
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+                link = "https://t.me/Repthon" + MUST_JOIN
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"**- عليـك الاشتـراك بقنـاة السـورس** [𝙕𝙚𝙙𝙏𝙝𝙤𝙣]({link}) **لكي تستطيـع استخـدام البــوت** 🧸♥️",
+                    f"**- عليـك الاشتـراك بقنـاة السـورس** [𝐑𝐄𝐏𝐓𝐇𝐎𝐍]({link}) **لكي تستطيـع استخـدام البــوت** 🧸♥️",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("اضغـط للاشتـراك", url=link)]
